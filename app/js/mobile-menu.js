32498ec4e96c5e64c2233f9df2eae602eb65e003
Ruslan <ruslan.spbrus@gmail.com>
2016-11-11 17:03:44 +0300

    переделал меню

--- app/js/mobile-menu.js
+++ app/js/mobile-menu.js
@@ -1,8 +1,10 @@
-(function() {
+;(function() {
+
   var menuBtn = document.querySelector('#mobile-menu-btn');
   var nav = document.querySelector('#main-nav');
 
-  menuBtn.addEventListener('click', function() {
+  menuBtn.onclick = function() {
     nav.classList.toggle('main-nav--show-menu');
-  });
+  };
+
 })();
